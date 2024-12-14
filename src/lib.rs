c451242c82bf5ff5b6e7ba6c6f472d55859a9c7b
Aje Damilola <ajedamilola2005@gmail.com>
2024-12-14 16:31:34 +0100

    Plenty fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
-
-/// Import items from the SDK. The prelude contains common traits and macros.
 use stylus_sdk::{alloy_primitives::U256, msg, prelude::*};
-
-// Define some persistent storage using the Solidity ABI.
-// `Counter` will be the entrypoint.
 sol_storage! {
     #[entrypoint]
     pub struct EventDB {
@@ -38,48 +33,41 @@ impl EventDB {
         new_event.title.set_str(title);
         new_event.description.set_str(description);
         new_event.date.set_str(date);
+        new_event.id.set(num);
         self.num_of_event.set(num + U256::from(1));
-        format!(
-            "{{\"title\":\"{}\",\"description\":\"{}\",\"date\":\"{}\",\"date\":\"{}\"}}",
-            new_event.title.get_string(),
-            new_event.description.get_string(),
-            new_event.date.get_string(),
-            new_event.id.get(),
-        );
     }
 
-    pub fn list_all(&self) {
+    pub fn list_all(&self) -> String {
         let mut response = String::from("[");
         let max_str = self.num_of_event.to_string();
         let max = max_str.parse().unwrap();
         for i in 0..max {
             let event = self.events.get(U256::from(i));
             let str = format!(
-                "{{\"title\":\"{}\",\"description\":\"{}\",\"date\":\"{}\",\"date\":\"{}\"}},",
+                "{{\"title\":\"{}\",\"description\":\"{}\",\"date\":\"{}\",\"id\":\"{}\",\"owner\":\"{}\"}},",
                 event.title.get_string(),
                 event.description.get_string(),
                 event.date.get_string(),
                 event.id.get(),
+                event.owner.get()
             );
             response.push_str(str.as_str());
-            if i != max {
-                response.push_str(",");
-            }
         }
         response.push_str("]");
 
-        drop(response);
+        response
     }
-    pub fn get_event(&mut self, id: U256) {
-        let event = self.events.get(id);
-        format!(
-            "{{\"title\":\"{}\",\"description\":\"{}\",\"date\":\"{}\",\"date\":\"{}\"}}",
-            event.title.get_string(),
-            event.description.get_string(),
-            event.date.get_string(),
-            event.id.get()
-        );
-    }
+    // pub fn get_event(&mut self, id: U256) -> String {
+    //     let event = self.events.get(id);
+    //     let response = format!(
+    //         "{{\"title\":\"{}\",\"description\":\"{}\",\"date\":\"{}\",\"id\":\"{}\",}}",
+    //         event.title.get_string(),
+    //         event.description.get_string(),
+    //         event.date.get_string(),
+    //         event.id.get(),
+    //     );
+    //     response
+    // }
 
     pub fn register_event(&mut self, id: U256, name: String) {
         let mut event = self.events.setter(id);
@@ -89,11 +77,9 @@ impl EventDB {
         new_attendee.id.set(num);
         new_attendee.name.set_str(name);
         event.num_of_attendees.set(num + U256::from(1));
-
-        true;
     }
 
-    pub fn list_attendees(&mut self, id: U256) {
+    pub fn list_attendees(&self, id: U256) -> String {
         let event = self.events.get(id);
         let mut response = String::from("[");
         let max_str = event.num_of_attendees.to_string();
@@ -106,10 +92,8 @@ impl EventDB {
                 attendee.name.get_string()
             );
             response.push_str(str.as_str());
-            if i != max {
-                response.push_str(",");
-            }
         }
         response.push_str("]");
+        response
     }
 }
