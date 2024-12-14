94a4362243016a0879d745e2a8dc0e1db43560dc
Aje Damilola <ajedamilola2005@gmail.com>
2024-12-14 13:54:55 +0100

    Complete contract

--- src/lib.rs
+++ src/lib.rs
@@ -92,9 +92,18 @@ impl EventDB {
         }
         response.push_str("]");
 
-        response;
+        drop(response);
     }
-    pub fn get_event(&mut self, id: U256) {}
+    pub fn get_event(&mut self, id: U256) {
+        let event = self.events.get(id);
+        format!(
+            "{{\"title\":\"{}\",\"description\":\"{}\",\"date\":\"{}\",\"date\":\"{}\"}}",
+            event.title.get_string(),
+            event.description.get_string(),
+            event.date.get_string(),
+            event.id.get()
+        );
+    }
 
     pub fn register_event(&mut self, id: U256, name: String) {
         let mut event = self.events.setter(id);
@@ -106,5 +115,25 @@ impl EventDB {
         event.num_of_attendees.set(num + U256::from(1));
 
         true;
+    }
+
+    pub fn list_attendees(&mut self, id: U256) {
+        let event = self.events.get(id);
+        let mut response = String::from("[");
+        let max_str = event.num_of_attendees.to_string();
+        let max = max_str.parse().unwrap();
+        for i in 0..max {
+            let attendee = event.attendees.get(U256::from(i));
+            let str = format!(
+                "{{\"id\":\"{}\",\"name\":\"{}\"}},",
+                attendee.id.get(),
+                attendee.name.get_string()
+            );
+            response.push_str(str.as_str());
+            if i != max {
+                response.push_str(",");
+            }
+        }
+        response.push_str("]");
     }
 }
